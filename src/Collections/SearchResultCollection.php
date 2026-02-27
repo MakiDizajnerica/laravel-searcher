64443558f33e8eab6071927135b6baa15979b84a
MakiDizajnerica <n.marijanovic@hotmail.com>
2026-02-27 16:49:23 +0100

    Refactor addResults method in SearchResultCollection to improve type safety and handling of empty results

--- src/Collections/SearchResultCollection.php
+++ src/Collections/SearchResultCollection.php
@@ -2,26 +2,29 @@
 
 namespace MakiDizajnerica\Searcher\Collections;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class SearchResultCollection extends Collection
 {
     /**
      * Add search results to collection.
-     * 
+     *
      * @param  \Illuminate\Database\Eloquent\Collection $results
      * @return void
      */
-    public function addResults(Collection $results): void
+    public function addResults(EloquentCollection $results): void
     {
-        $this->items = $this->merge(
-            $results->pipe(fn ($results) => $results->all())
-        );
+        if ($results->isEmpty()) {
+            return;
+        }
+
+        $this->push(...$results->all());
     }
 
     /**
      * Group results by their defined search type.
-     * 
+     *
      * @return $this
      */
     public function groupBySearchType(): static
